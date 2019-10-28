01782f0d7a37ceaa4c2777a4e6af011ceb17e3c0
Hanna Lee <mani21na@gmail.com>
2019-10-28 17:34:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get ('/name') {}
+  
+  get ('/hometown') {}
+
+  get ('/favorite-song') {}
+
 end
