1bae8562737125da2e1afad97c36f241bd4c69d8
Hanna Lee <mani21na@gmail.com>
2019-10-28 17:37:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get ('/name') {"My name is Mani"}
+  
+  get ('/hometown') {"My hometown is Seoul"}
+
+  get ('/favorite-song') {"My favorite song is Gayo"}
 end
